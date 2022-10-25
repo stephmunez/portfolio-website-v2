569da7e292e82bbebb874af6dfd6562cb1c566f0
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-10-25 13:34:53 +0800

    setup: head

---
+++ atoms/icons/Hamburger/Hamburger.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hamburger = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='13'>
+      <g fill='#CCD6F6' fillRule='evenodd'>
+        <path d='M0 0h24v1H0zM0 6h24v1H0zM0 12h24v1H0z' />
+      </g>
+    </svg>
+  );
+};
+
+export default Hamburger;
---
+++ components/PageHead/PageHead.js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+const PageHead = ({ children }) => {
+  return (
+    <>
+      <Head>
+        {children}
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicons/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicons/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='/favicons/safari-pinned-tab.svg'
+          color='#0A192F'
+        />
+        <link rel='shortcut icon' href='/favicons/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#0a192f' />
+        <meta
+          name='msapplication-config'
+          content='/favicons/browserconfig.xml'
+        />
+        <meta name='theme-color' content='#0a192f' />
+        <meta name='description' content='Stephen Joseph portfolio website' />
+      </Head>
+    </>
+  );
+};
+
+export default PageHead;
