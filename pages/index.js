e5f5656dc28f63d45d43618d41182754482cb597
Stephen Muñez <sjtmunez@gmail.com>
2024-02-12 13:23:14 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,31 @@ const Home = () => {
     <>
       <PageHead>
         <title>Stephen Joseph | Frontend Developer</title>
+        {/* SEO */}
+        <meta
+          name='description'
+          content="I'm a frontend developer who loves building engaging digital experiences. Currently, I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript"
+        />
+        <link rel='canonical' href='https://stephjoseph.dev/' />
+        <meta
+          name='robots'
+          content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
+        />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:title'
+          content='Stephen Joseph | Frontend Developer'
+        />
+        <meta
+          property='og:description'
+          content="I'm a frontend developer who loves building engaging digital experiences. Currently, I focus on writing accessible HTML, using modern CSS practices and writing clean JavaScript"
+        />
+        <meta property='og:url' content='https://stephjoseph.dev/' />
+        <meta property='og:site_name' content="Stephen Joseph's Website" />
+        <meta
+          property='og:image'
+          content='/images/homepage/image-homepage-profile@2x.jpg'
+        />
       </PageHead>
 
       <Layout>
