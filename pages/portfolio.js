ceeded9693b530473dc4a049593236664d1bd7fe
Stephen Muñez <sjtmunez@gmail.com>
2024-03-25 17:47:21 +0800

    Update portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -34,8 +34,9 @@ const Portfolio = ({ projects }) => {
         <main className='flex w-full flex-col items-center overflow-hidden'>
           <h1 className='invisible absolute'>Portfolio</h1>
           <h2 className='font-body-1 mb-12 w-[82.93%] text-slate md:w-[89.71%] xl:w-[77.08%]'>
-            **These are some of my personal projects, while work projects are
-            available upon request..
+            **These are some of my personal projects that I keep adding to
+            regularly to expand my skills and to try out new tech, while work
+            projects are available upon request..
           </h2>
           <div className='mb-20 flex w-[82.93%] flex-col items-center gap-[4.5rem] md:mb-32 md:w-[89.71%] md:gap-24 xl:mb-40 xl:w-[77.08%] xl:gap-32'>
             {projects.map((project, index) => (
