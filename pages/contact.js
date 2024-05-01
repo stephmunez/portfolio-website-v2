584baa9f7f1ca95d423e84525a3166bff47b22fa
Stephen Muñez <sjtmunez@gmail.com>
2024-05-01 14:38:33 +0800

    feat: contact tablet

--- pages/contact.js
+++ pages/contact.js
@@ -33,9 +33,9 @@ const Contact = () => {
       <Layout>
         <main className='flex w-full flex-col items-center gap-8 overflow-hidden'>
           <h1 className='invisible absolute'>Contact Me</h1>
-          <section className='flex w-[82.93%] flex-col gap-1 border-y border-solid border-aquamarine py-6'>
+          <section className='flex w-[82.93%] flex-col gap-1 border-y border-solid border-aquamarine py-6 md:w-[89.71%] md:gap-4'>
             <h2 className='font-h2 text-aquamarine'>Get in Touch</h2>
-            <div className='flex w-full flex-col gap-[0.875rem]'>
+            <div className='flex w-full flex-col gap-[0.875rem] md:gap-4'>
               <p className='font-body-1 text-light-slate'>
                 I’d love to hear about what you’re working on and how I could
                 help. I’m currently looking for a new role and am open to a wide
@@ -65,7 +65,7 @@ const Contact = () => {
               </ul>
             </div>
           </section>
-          <section className='mb-20 flex w-[82.93%] flex-col gap-6'>
+          <section className='mb-20 flex w-[82.93%] flex-col gap-6 md:mb-24 md:w-[89.71%] md:gap-8'>
             <h2 className='font-h2 text-aquamarine'>Contact Me</h2>
             <form
               ref={form}
@@ -73,29 +73,47 @@ const Contact = () => {
               className='flex w-full flex-col gap-6'
             >
               <div className='flex flex-col gap-2'>
-                <label className='font-body-1 text-light-slate'>Name</label>
+                <label
+                  className='font-body-1 w-max text-light-slate'
+                  for='user_name'
+                >
+                  Name
+                </label>
                 <input
                   type='text'
                   name='user_name'
+                  id='user_name'
                   className='placeholder:font-body-1 font-body-1 bg-light-slate/10 px-4 py-2 text-light-slate caret-aquamarine placeholder:text-light-slate/40 focus:outline-none'
                   placeholder='Jane Appleseed'
                 />
               </div>
 
               <div className='flex flex-col gap-2'>
-                <label className='font-body-1 text-light-slate'>Email</label>
+                <label
+                  className='font-body-1 w-max text-light-slate'
+                  for='user_email'
+                >
+                  Email
+                </label>
                 <input
                   type='email'
                   name='user_email'
+                  id='user_email'
                   className='placeholder:font-body-1 font-body-1 bg-light-slate/10 px-4 py-2 text-light-slate caret-aquamarine placeholder:text-light-slate/40 focus:outline-none'
                   placeholder='email@example.com'
                 />
               </div>
 
               <div className='flex flex-col gap-2'>
-                <label className='font-body-1 text-light-slate'>Message</label>
+                <label
+                  className='font-body-1 w-max text-light-slate'
+                  for='message'
+                >
+                  Message
+                </label>
                 <textarea
                   name='message'
+                  id='message'
                   rows='5'
                   cols='32'
                   className='placeholder:font-body-1 font-body-1 bg-light-slate/10 px-4 py-2 text-light-slate caret-aquamarine placeholder:text-light-slate/40 focus:outline-none'
